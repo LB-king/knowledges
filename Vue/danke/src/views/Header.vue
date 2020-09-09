d8ab46b8b3a933c2997ecc14369dfd9cb502b017
dingxl <dingxiliang@knowlegene.com>
2020-09-09 10:35:57 +0800

    修改md信息

--- Vue/danke/src/views/Header.vue
+++ Vue/danke/src/views/Header.vue
@@ -1,5 +1,20 @@
-<template>
-  <div class="about">
-    <h1>公共的头部hahha</h1>
-  </div>
+<template lang="pug">
+  el-header.header_wrapper
+    el-row(:gutter="10")
+      | 用户信息
 </template>
+<style lang="scss">
+.header_wrapper {
+  height: 60px;
+  line-height: 60px;
+  background: linear-gradient(
+    to right,
+    rgb(210, 230, 241),
+    rgb(145, 181, 247),
+    rgba(84, 110, 179, 0.8)
+  );
+  .bg-red {
+    background-color: #bbffcc;
+  }
+}
+</style>
