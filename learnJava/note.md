c73dd29f41579c75c9f17c1c932e734d6aad7322
LBking <731540792@qq.com>
2022-08-23 21:57:51 +0800

    update mac

---
+++ learnJava/note.md
@@ -0,0 +1,10 @@
+### 1.环境配置
+
+https://www.oracle.com/java/technologies/downloads/#java8-mac
+
+下载JDK需要登录oracle账号
+
+java -jar ureport-keep-0.0.1-SNAPSHOT.jar
+
+http://localhost:8888/ureport_keep/designer/designer
+
--- nodeJs/expressApp/routes/users.js
+++ nodeJs/expressApp/routes/users.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let data = {
+    code: 1,
+    list: [
+      {
+        name: 'zs',
+        age: 26
+      },
+      {
+        name: 'gy',
+        age: 34
+      }
+    ]
+  }
+  res.send(data);
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
